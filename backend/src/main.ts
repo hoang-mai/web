75bfd8a797a6f920843589a00f775c7bba736449
hoang-mai <maianhhoang31072003@gmail.com>
2025-03-20 22:23:01 +0700

    catch http Exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpAdapterHost } from '@nestjs/core';
-import { HttpExceptionFilter } from './filter/http-exception.filter';
+import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // This is the global filter that will catch all exceptions
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
