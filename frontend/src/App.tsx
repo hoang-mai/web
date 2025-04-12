a6c2cda50341417a0031228ec21fb7ca84e8f417
mrBunachi <hoangtsugumi194@gmail.com>
2025-04-12 18:55:08 +0700

    wip: code button chọn địa chỉ và lưu vào storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
-function App() {
+import Header from './components/Header'
+import Footer from './components/Footer'
+import LocationSelector from "./components/LocationSelector";
 
+function App() {
+  function App() {
+    return (
+      <div className="p-4">
+        <LocationSelector />
+      </div>
+    );
+  };
   return (
-    <div>
-      
-        
-   
-     
+    <div className="bg-white text-black min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        {/* Nội dung trang chính sẽ ở đây */}
+      </main>
+      <Footer />
     </div>
   )
 }
