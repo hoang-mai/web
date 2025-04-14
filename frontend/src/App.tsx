0887bcdb0aeb83a2e519fb69031f0668021cc4f9
hoang-mai <maianhhoang31072003@gmail.com>
2025-04-14 17:11:02 +0700

    hot fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,12 @@ import Footer from './components/Footer'
 import LocationSelector from "./components/LocationSelector";
 
 function App() {
-  function App() {
-    return (
-      <div className="p-4">
-        <LocationSelector />
-      </div>
-    );
-  };
   return (
     <div className="bg-white text-black min-h-screen flex flex-col">
       <Header />
+      <div className="p-4">
+        <LocationSelector />
+      </div>
       <main className="flex-grow">
         {/* Nội dung trang chính sẽ ở đây */}
       </main>
